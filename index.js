211d9c9f2649108cfe33a54b1645bc119dc4a755
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-02-11 11:14:53 -0500

    Removed unnecessary delay

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
-const website = 'https://www.bestbuy.ca/en-ca/product/samsung-hw-b450-zc-300-watt-2-1-channel-sound-bar-with-wireless-subwoofer/16000037';
+const website = 'https://www.bestbuy.ca/en-ca/product/sonos-arc-sound-bar-black/14597172';
 
 // puppeteer tutorial https://youtube.com/watch?v=lgyszZhAZOI&feature=shares
 async function start() {
@@ -8,9 +8,7 @@ async function start() {
     const page = await browser.newPage();
     await page.goto(website);
     await page.click('#reviews');
-    // currently doesn't wait long enough
-    // await page.waitForSelector('.reviewTitle_1qq1j span', {visible: true});
-    await delay(2000);
+    await page.waitForSelector('.reviewTitle_1qq1j span', {visible: true});
     await page.screenshot({ path: "bestbuy.png", fullPage: true });
     const reviews = await page.evaluate(() => {
         return Array.from(document.querySelectorAll(".reviewContent_XCspv p span")).map(x => x.textContent)
@@ -21,9 +19,3 @@ async function start() {
 }
 
 start();
-
-function delay(time) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, time)
-    });
-}
