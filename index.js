d36828c1989f97361845dc07f4c96483b3119742
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-02-11 16:07:34 -0500

    Combined AMAZ and BBY to one function

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
-// const website = 'https://www.bestbuy.ca/en-ca/product/sonos-arc-sound-bar-black/14597172';
-const website = 'https://www.amazon.ca/dp/B09F1QQZM2';
+const testWebsite = 'https://www.bestbuy.ca/en-ca/product/sonos-arc-sound-bar-black/14597172';
+// const testWebsite = 'https://www.amazon.ca/dp/B09F1QQZM2';
 
 // puppeteer tutorial https://youtube.com/watch?v=lgyszZhAZOI&feature=shares
-async function scrapeBestBuy() {
+function callThisOne(website) {
+    let reviewSelector;
+    if (website.includes('bestbuy')) {
+        reviewSelector = '.reviewContent_XCspv p span';
+    }
+    else if (website.includes('amazon')) {
+        reviewSelector = 'div.a-expander-content.reviewText.review-text-content.a-expander-partial-collapse-content > span';
+    }
+    (async () => {
+        console.log(await scrapeWebsite(website, reviewSelector))
+    })()
+}
+
+async function scrapeWebsite(website, reviewSelector) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(website);
-    await page.click('#reviews');
-    await page.waitForSelector('.reviewTitle_1qq1j span', {visible: true});
+    if (website.includes('bestbuy')) {
+        await page.click('#reviews');
+        await page.waitForSelector('.reviewTitle_1qq1j span', {visible: true});
+    }
     await page.screenshot({ path: "website.png", fullPage: true });
-    const reviews = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('.reviewContent_XCspv p span')).map(x => x.textContent)
-    });
-    console.log(reviews);
+    const reviews = await page.evaluate((reviewSelector) => {
+        return Array.from(document.querySelectorAll(reviewSelector)).map(x => x.textContent)
+    }, reviewSelector);
     await fs.writeFile('reviews.txt', reviews.join('\r\n'));
     await browser.close();
+    return reviews;
 }
 
-async function scrapeAmazon() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(website);
-    await page.screenshot({ path: "website.png", fullPage: true });
-    const reviews = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('div.a-expander-content.reviewText.review-text-content.a-expander-partial-collapse-content > span')).map(x => x.textContent)
-    });
-    console.log(reviews);
-    await fs.writeFile('reviews.txt', reviews.join('\r\n'));
-    await browser.close();
-}
-
-// scrapeBestBuy();
-scrapeAmazon();
+callThisOne(testWebsite);
